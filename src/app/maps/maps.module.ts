e34a04e1139a1edfed1e6ada3275b2ca3d7c0be5
Javier <javierruizmora50@gmail.com>
2023-06-13 16:49:14 +0200

    Vídeo 302 incluido

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import * as mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamF2aXJ1aXoiLCJhIjoiY2xpdGF3MmpnMDM1ZzNybzJhb2wwaXQzZyJ9.c8kd5t_41__EI09cIMhuOg';
+
+import { MapsRoutingModule } from './maps-routing.module';
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+
+
+@NgModule({
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent
+  ],
+  imports: [
+    CommonModule,
+    MapsRoutingModule
+  ]
+})
+export class MapsModule { }
